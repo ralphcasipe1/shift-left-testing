fbf1c2cd89d6225585ba431d6209d9cda503c4e0
Ralph Kevin Y. Casipe <ralphcasipe1@gmail.com>
2021-04-02 18:38:46 +0800

    feat: add new files, subtract

---
+++ src/common/subtract.test.js
@@ -0,0 +1,10 @@
+const { expect } = require('chai');
+
+const subtract = require('./subtract');
+
+describe('subtract', () => {
+  it('should have the correct difference ', () => {
+    expect(subtract(2, 2))
+      .to.equals(0);
+  });
+});
