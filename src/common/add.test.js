a6c1c874a99f1a393440d578e57b2adc8eb26ecf
Ralph Kevin Y. Casipe <ralphcasipe1@gmail.com>
2021-04-02 16:59:01 +0800

    feat: add and implement linting

--- src/common/add.test.js
+++ src/common/add.test.js
@@ -1,9 +1,9 @@
-const { expect } = require("chai");
+const { expect } = require('chai');
 
 const add = require('./add');
 
-describe('add', function () {
-  it('should have the correct sum', function () {
+describe('add', () => {
+  it('should have the correct sum', () => {
     expect(add(2, 2)).to.equals(4);
   });
 });
